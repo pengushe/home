4fa872a586617c9fd299a043d7c67afaa0d53f2d
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2021-01-15 00:56:22 -0600

    add file upload widget

--- pengushe/home/src/index.js
+++ pengushe/home/src/index.js
@@ -1,65 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
-import Project from './components/Project';
-import Sector from './components/Sector'
-import Image1 from './img/logo.png';
 import Axios from 'axios';
-const api = Axios.create({
-  baseURL: 'cloudinary://696971683611278:tpGXwE_UfxnpePkKt-UbQknzG7I@pengushe-home'
-});
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cloudName: "pengushe-home",
-      publicId: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({ publicId: event.target.value });
-    event.preventDefault();
-  }
-  render() {
-    return (
-      <div>
-        <input type="text"
-          value={this.state.publicId}
-          onChange={this.handleChange}
-        />
-        <h3>{}</h3>
-          <Sector id="logo">
-            <Image publicId={this.state.publicId} crop="scale" />
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
-          <Sector id="网页" className="invisible">
-            <Project id={Image1} imgPath={Image1} imgName={Image1} />
-          </Sector>
-          <Sector id="名片" className="invisible">
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
-          <Sector id="宣传海报" className="invisible">
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
-          <Sector id="产品包装" className="invisible">
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
-          <Sector id="期刊设计" className="invisible">
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
-          <Sector id="电商设计" className="invisible">
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
-          <Sector id="影楼后期" className="invisible">
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
-          <Sector id="影视后期" className="invisible">
-            <Project id="logo1" imgPath={Image1} imgName="logo1" />
-          </Sector>
+// import useCallback from 'react';
+// import { useDropzone } from 'react-dropzone'
+// import styles from './style/App.css';
+// import Project from './components/Project';
+// import Sector from './components/Sector'
+// import Image1 from './img/logo.png';
 
+function App() {
+  // select the image
+  const [imageSelect, setImageSelected] = useState("");
+  // parameter for uploading to Cloudinary
+  const uploadImage = () => {
+    const formData = new FormData();
+    formData.append("file", imageSelect);
+    formData.append("upload_preset", "ugployuy");
+    Axios.post(
+      'https://api.cloudinary.com/v1_1/pengushe-home/image/upload',
+      formData
+    ).then((response) => { window.alert("你文件已经传送到数据库里：）") });
+  }
+  // // select the image in Dropzoon and upload to Cloudinary
+  // const onDrop = useCallback(acceptedFiles => {
+  //   const formData = new FormData();
+  //   formData.append("file", acceptedFiles);
+  //   formData.append("upload_preset", "ugployuy");
+  //   Axios.post(
+  //     'https://api.cloudinary.com/v1_1/pengushe-home/image/upload',
+  //     formData
+  //   ).then((response) => { window.alert("你上传的文件已经传送到数据库里！") });
+  //   console.log(acceptedFiles);
+  // }, [])
+  // const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  //   onDrop,
+  //   accept: "image/*",
+  //   multiple: false,
+  // });
+
+  return (
+    <div>
+      {/* <div
+        {...getRootProps()}
+        className={`${styles.dropzone} ${isDragActive ? styles.active : null}`}
+      >
+        <input {...getInputProps()} />
+        Drop Zone
+      </div> */}
+      <div className="form form-inline">
+        <input type="file" onChange={(event) => {
+          setImageSelected(event.target.files[0]);
+        }} />
+        <button className="btn btn-primary" onClick={uploadImage}>upload image</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
-ReactDom.render(<App />, document.getElementById('root'));
 
+ReactDom.render(<App />, document.getElementById('root'));
