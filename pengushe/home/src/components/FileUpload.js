6df6e7b1f1576c45c4fb53005b27b0aa6855ea2e
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2021-01-31 01:04:53 -0600

    upload page update

--- pengushe/home/src/components/FileUpload.js
+++ pengushe/home/src/components/FileUpload.js
@@ -1,27 +1,46 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
 import '../style/App.css';
+import Highlight from 'react-highlight';
 // import useCallback from 'react';
 // import { useDropzone } from 'react-dropzone'
 // import Project from './components/Project';
 // import Sector from './components/Sector'
 // import Image1 from './img/logo.png';
 
-function FileUpload() {
-  // ready for upload
-  const [imageReady, setImageReady] = useState("");
+var upload_preset = "work_img";
+const FileUpload = (props) => {
+  // display public_id and secure_url
+  const [url, setURL] = useState("");
   // select the image
-  const [imageSelect, setImageSelected] = useState("");
+  const [imageSelect, setImageSelected] = useState("将这个URL送给代码组：）");
   // parameter for uploading to Cloudinary
-  const uploadImage = () => {
-    const formData = new FormData();
-    formData.append("file", imageSelect);
-    formData.append("resource_type", "auto");
-    formData.append("upload_preset", "ugployuy");
-    Axios.post(
-      'https://api.cloudinary.com/v1_1/pengushe-home/image/upload',
-      formData
-    ).then((response) => { window.alert("你的文件已经传送到数据库里：）") });
+  const uploadImage = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", imageSelect);
+      formData.append("resource_type", "auto");
+      formData.append("upload_preset", upload_preset);
+      Axios.post(
+        'https://api.cloudinary.com/v1_1/pengushe-home/image/upload',
+        formData
+      ).then(
+        (response) => {
+          window.alert("你的文件已经传送到数据库里：）");
+          setURL(response.data.secure_url);
+        });
+    } catch (error) {
+      window.alert("文件没有成功上传，可能是你的文件超过了10M");
+    }
+  }
+
+  function changeImgType(event) {
+    if (event.target.value == "option1") {
+      upload_preset = "profile_img";
+    }
+    else {
+      upload_preset = "work_img";
+    }
   }
   // // select the image in Dropzoon and upload to Cloudinary
   // const onDrop = useCallback((acceptedFiles) => {
@@ -41,24 +60,39 @@ function FileUpload() {
 
   return (
     <div>
-      <h1 hidden>文件已经准备好上传，点击上传键</h1>
-      <div className="input-group">
-        <div className="custom-file">
-          <input type="file" className="custom-file-input" id="文件上传"
-            onChange={(event) => {
-              setImageSelected(event.target.files[0]);
-              window.alert("文件已经准备好上传，点击上传键，如果想换一个图片上传，点击OK，然后请先刷新界面");
-            }}
-          />
-          <label className="custom-file-label" htmlFor="文件上传">选择文件</label>
+      <div className="container" style={{ marginTop: "2%" }}>
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"
+            onChange={changeImgType} />
+          <label className="form-check-label" for="inlineRadio1">上传个人图像</label>
         </div>
-        <div className="input-group-append">
-          <button className="btn btn-outline-secondary" type="button"
-            onClick={uploadImage}
-          >点击上传</button>
+        <div className="form-check form-check-inline">
+          <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"
+            onChange={changeImgType} />
+          <label className="form-check-label" for="inlineRadio2">上传个人素材</label>
         </div>
-      </div>
-      <div>
+        <h1 hidden>文件已经准备好上传，点击上传键</h1>
+        <div className="input-group">
+          <div className="custom-file">
+            <input type="file" className="custom-file-input" id="文件上传"
+              onChange={(event) => {
+                setImageSelected(event.target.files[0]);
+                window.alert("文件已经准备好上传，点击上传键，如果想换一个图片上传，点击OK，然后请先刷新界面");
+              }}
+            />
+            <label className="custom-file-label" htmlFor="文件上传">选择文件</label>
+          </div>
+          <div className="input-group-append">
+            <button className="btn btn-outline-secondary" type="button"
+              onClick={uploadImage}
+            >点击上传</button>
+            <button className="btn btn-sm btn-outline-secondary" type="button"
+              onClick={() => props.selectValue.setValue(url)}
+            >显示图片的URL</button>
+          </div>
+        </div>
+        <div>
+        </div>
       </div>
     </div>
   );
--- pengushe/home/src/index.js
+++ pengushe/home/src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import { Image, Video, Transformation, CloudinaryContext } from 'cloudinary-react';
 import FileUpload from './components/FileUpload';
 import 'bootstrap/dist/css/bootstrap.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,43 +11,63 @@ class App extends React.Component {
     this.state = {
       cloudName: "pengushe-home",
       publicId: 'sample',
-      width : '0.5',
-      crop : 'scale'
+      width: '0.5',
+      crop: 'scale',
+      value: '',
+      copied: false,
     };
     this.filterID = this.filterID.bind(this);
     this.changWidth = this.changWidth.bind(this);
-
   }
-  filterID(event){
-    this.setState({publicId: event.target.value});
+  filterID(event) {
+    this.setState({ publicId: event.target.value });
     event.preventDefault();
   }
-  changWidth(event){
-    this.setState({width: event.target.value});
+  changWidth(event) {
+    this.setState({ width: event.target.value });
     event.preventDefault();
+  }
+  setValue(value) {
+    this.setState({ value: value });
   }
   render() {
     return (
       <div>
         {/* 上传文件 */}
-        <FileUpload />
+        <FileUpload selectValue={
+          { setValue: this.setValue.bind(this) }
+        } />
+        <div>
+          <div class="mb-3">
+            <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"
+              value={this.state.value}
+              onChange={({ target: { value } }) => this.setState({ value, copied: false })} readOnly />
+            <CopyToClipboard text={this.state.value}
+              onCopy={() => this.setState({ copied: true })}>
+              <button>复制图片的URL</button>
+            </CopyToClipboard>
+          </div>
+
+
+          {this.state.copied ? <span style={{ color: 'red' }}>Copied.</span> : null}
+        </div>
         {/* -end- */}
         <div className="input-group mb-3">
           <div className="input-group-prepend">
             <span className="input-group-text" id="inputGroup-sizing-default">查看已经上传的图片</span>
           </div>
-          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={this.filterID}/>
+          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={this.filterID} />
           <div className="input-group-prepend">
             <span className="input-group-text" id="inputGroup-sizing-default">更改大小 0.5(%) 或者 200 px</span>
           </div>
-          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={this.changWidth}/>
-          
+          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={this.changWidth} />
+
         </div>
         <CloudinaryContext cloudName={this.state.cloudName}>
           <Image publicId={this.state.publicId} width={this.state.width} crop="scale" />
         </CloudinaryContext>
         <p> 已经上传的图片
-          youyou2,donggua1,test1,test2,test3
+          youyou1,youyou2,donggua1,test1,test2,test3
         </p>
       </div>
     );
