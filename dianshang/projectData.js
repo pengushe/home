6010b501a6fd951962a7d9ccd52260f0851e40e5
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2021-05-27 18:52:01 -0500

    add a strapi backend

--- dianshang/projectData.js
+++ dianshang/projectData.js
@@ -1,4 +1,14 @@
 // 电商作品
-let ecommerce =  [
+//fetch data from API
+const data = () => {
+  axios.get('https://pengushe.herokuapp.com/products')
+  .then(response => {
+      console.log(response)
+      .catch(erro => { console.log(erro) }
+      );
+    });
+
+}
+const ecommerce = [
   "https://res.cloudinary.com/pengushe-home/image/upload/w_300,f_auto/v1617844194/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20210408090109.jpg",
 ]
